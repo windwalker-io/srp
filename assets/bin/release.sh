96f4f5687f70da0de44644331d4ccb8e06707f66
Simon Asika <asika32764@gmail.com>
2023-11-17 16:16:23 +0800

    release script

--- assets/bin/release.sh
+++ assets/bin/release.sh
@@ -1,5 +1,5 @@
 yarn build:prod
+V=$(npm version "${1:-patch}" --no-git-tag-version)
 git add .
-git commit -am "Prepare for release."
-npm version "${1:-patch}"
+git commit -am "NPM release ${V}"
 npm publish
